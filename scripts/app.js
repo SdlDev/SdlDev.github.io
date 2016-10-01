083465b9a437d55a36aa11704a66256608bcd8b6
Kamipown <paul.delobbel@gmail.com>
2016-10-01 07:22:39 +0200

    Header + nav

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function($routeProvider)
+{
+	$routeProvider.when("/", {templateUrl: "partials/home.html"});
+	$routeProvider.otherwise({templateUrl: "partials/404.html"});
+});
