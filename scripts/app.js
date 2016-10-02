7f634a377c6dca1e20da414d98e8fae012c37bb9
Kamipown <paul.delobbel@gmail.com>
2016-10-02 06:40:31 +0200

    home + 404 redirection

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,5 @@ var app = angular.module("app", ["ngRoute"]);
 app.config(function($routeProvider)
 {
 	$routeProvider.when("/", {templateUrl: "partials/home.html"});
-	$routeProvider.otherwise({templateUrl: "partials/404.html"});
+	$routeProvider.otherwise({templateUrl: "partials/404.html", controller: "404_ctrl"});
 });
---
+++ scripts/controllers/404_ctrl.js
@@ -0,0 +1,21 @@
+app.controller("404_ctrl", function($scope, $interval, $location)
+{
+	$scope.remaining_time = "5 secondes";
+	var sec = 4;
+	var int;
+
+	$scope.deduct = function()
+	{
+		int = $interval(function()
+		{
+			$scope.remaining_time = sec > 1 ? sec + " secondes" : sec + " seconde";
+			if (sec == 0)
+			{
+				$interval.cancel(int);
+				$location.path("/");
+			}
+			--sec;
+		}, 1000);
+	}
+	$scope.deduct();
+});
