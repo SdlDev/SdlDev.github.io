c795024e9d0d2be8a3b9558b5362f504bda91c01
Kamipown <paul.delobbel@gmail.com>
2016-10-03 00:02:36 +0200

    mobile menu

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+var menu_displayed = false;
+
+function toggle_mobile_menu(caller)
+{
+	if (menu_displayed)
+		document.getElementById("mobile_nav").style.maxHeight = "0px";
+	else
+		document.getElementById("mobile_nav").style.maxHeight = "calc(100% - 64px)";
+	caller.classList.toggle("active");
+	menu_displayed = !menu_displayed;
+}
