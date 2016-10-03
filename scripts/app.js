e84f63d0da5be5fe10799fd200fd137b31ddb5d9
Kamipown <paul.delobbel@gmail.com>
2016-10-04 01:17:03 +0200

    first post + disqus

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,6 @@ var app = angular.module("app", ["ngRoute"]);
 app.config(function($routeProvider)
 {
 	$routeProvider.when("/", {templateUrl: "partials/home.html"});
+	$routeProvider.when("/tutorials", {templateUrl: "partials/tutorials.html"});
 	$routeProvider.otherwise({templateUrl: "partials/404.html", controller: "404_ctrl"});
 });
